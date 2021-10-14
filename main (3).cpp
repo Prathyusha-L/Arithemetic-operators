3e2638d6505b463f026685de9aadd70ee76b1223
L Prathyusha <83442712+Prathyusha-L@users.noreply.github.com>
2021-10-14 10:54:26 +0530

    Add files via upload

---
+++ main (3).cpp
@@ -0,0 +1,29 @@
+// C program to demonstrate
+// working of binary arithmetic
+// operators
+#include <stdio.h>
+
+int main()
+{
+	int a = 10, b = 4, res;
+
+	// printing a and b
+	printf("a is %d and b is %d\n", a, b);
+
+	res = a + b; // addition
+	printf("a+b is %d\n", res);
+
+	res = a - b; // subtraction
+	printf("a-b is %d\n", res);
+
+	res = a * b; // multiplication
+	printf("a*b is %d\n", res);
+
+	res = a / b; // division
+	printf("a/b is %d\n", res);
+
+	res = a % b; // modulus
+	printf("a%%b is %d\n", res);
+
+	return 0;
+}
